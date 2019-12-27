d70ce55178b7816dc22d5efaa9bbe8e4e5502ab7
JSH <wntngns23@gmail.com>
2019-12-27 19:19:41 +0900

    Update Interface

--- hello/hello.go
+++ hello/hello.go
@@ -1,9 +1,16 @@
 package main
 
-import _ "GO_Alpaca/fizzbuzz"
-import _ "GO_Alpaca/errorwork"
-import _ "GO_Alpaca/example"
-import "GO_Alpaca/unicode"
+import (
+	_ "GO_Alpaca/errorwork"
+	_ "GO_Alpaca/fizzbuzz"
+
+	_ "GO_Alpaca/example"
+	"GO_Alpaca/unicode"
+)
+
+type selecter interface {
+	GetName() string
+}
 
 func main() {
 	unicode.Execute()
